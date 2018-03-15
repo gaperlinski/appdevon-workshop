7e6866aca6f3eb523c9d1bf88645ae264a83aed5
gaperlinski <gaperlinski@gmail.com>
2018-03-15 11:43:16 +0100

    static vs dynamic dispatch

--- HighPerformanceSwift/HighPerformanceSwift/main.swift
+++ HighPerformanceSwift/HighPerformanceSwift/main.swift
@@ -8,32 +8,41 @@
 
 import Foundation
 
-struct Wombat {
-    var name = "Bob"
-    var meh1 = NSAttributedString()
-    var meh2 = NSAttributedString()
-    var meh3 = NSAttributedString()
+protocol SomeProtocol {
+    var site: String { get set }
 }
 
-var item = Wombat()
-print("Step 1", CFGetRetainCount(item))
-
-let baz = item
-print("Step 2", CFGetRetainCount(item))
-
-let array = [item, baz]
-print("Step 3", CFGetRetainCount(item))
-
-func doStuff(with item: Wombat) {
-    // RETAIN
-    print("Step 4", CFGetRetainCount(item))
-    // RELEASE
+struct MyType: SomeProtocol {
+    var site: String = "Apple.com"
 }
 
-doStuff(with: item)
+func doA(_ thing: SomeProtocol) {
+    doB(thing)
+}
 
-for thing in array {
-    // RETAIN
-    print("Step 5", CFGetRetainCount(item))
-    // RELEASE
+func doB(_ thing: SomeProtocol) {
+    doC(thing)
+}
+
+func doC(_ thing: SomeProtocol) {
+    print(thing.site)
+}
+
+// Dynamic dispatch
+//func doThing(value: SomeProtocol) {
+//    print(value.site)
+//}
+
+// Static dispatch
+// Will attempt to create a specialized function at compile type
+// e.g. doThing(value: MyType)
+func doThing<T: SomeProtocol>(value: T) {
+    print(value.site)
+}
+
+var items: [MyType] = [MyType()]
+
+for _ in 1...1000 {
+//    doA(items[0])
+    doThing(value: items[0])
 }
