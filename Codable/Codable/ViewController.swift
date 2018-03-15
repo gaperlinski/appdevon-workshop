f74d44ae593ac98e37091fb34bed4b36dc8585ce
gaperlinski <gaperlinski@gmail.com>
2018-03-15 16:36:25 +0100

    codable start

---
+++ Codable/Codable/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Codable
+//
+//  Created by Paul Hudson on 23/10/2017.
+//  Copyright © 2017 Paul Hudson. All rights reserved.
+//
+
+import Cocoa
+
+struct Entry {
+    var url: String
+}
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadJSON()
+    }
+
+    func loadJSON() {
+
+    }
+}
+
