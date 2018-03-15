c10bf466e061284a9f5fc023b3712fc3df4e208b
gaperlinski <gaperlinski@gmail.com>
2018-03-15 11:26:58 +0100

    class retain counts

--- HighPerformanceSwift/HighPerformanceSwift/main.swift
+++ HighPerformanceSwift/HighPerformanceSwift/main.swift
@@ -8,21 +8,29 @@
 
 import Foundation
 
-protocol SomeProtocol {
-    
+class Wombat {
+    var name = "Bob"
 }
 
-struct MyType: SomeProtocol {
-    let url: URL
-    let id: UUID
-    var site: String
+var item = Wombat()
+print("Step 1", CFGetRetainCount(item))
+
+let baz = item
+print("Step 2", CFGetRetainCount(item))
+
+let array = [item, baz]
+print("Step 3", CFGetRetainCount(item))
+
+func doStuff(with item: Wombat) {
+    // RETAIN
+    print("Step 4", CFGetRetainCount(item))
+    // RELEASE
 }
 
-var urls = [SomeProtocol]()
+doStuff(with: item)
 
-for _ in 1...1000 {
-    let item = MyType(url: URL(string: "https://www.apple.com")!,
-                      id: UUID(),
-                      site: "Apple.com")
-    urls.append(item)
+for thing in array {
+    // RETAIN
+    print("Step 5", CFGetRetainCount(item))
+    // RELEASE
 }
