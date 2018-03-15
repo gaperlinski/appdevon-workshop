03574deb9326132b235ae7cb88daeb1b41bd6e25
gaperlinski <gaperlinski@gmail.com>
2018-03-15 17:01:32 +0100

    codable finish

--- Codable/Codable/ViewController.swift
+++ Codable/Codable/ViewController.swift
@@ -10,23 +10,37 @@ import Cocoa
 
 struct Entry: Decodable {
     
-    struct Commit: Decodable {
-        struct Author: Decodable {
-            var name: String
-            var email: String
-            var date: String
-        }
-        var author: Author
-    }
-    
     var url: URL
     var htmlURL: URL
-    var commit: Commit
+    var name: String
+    var email: String
+    var date: Date
     
     enum CodingKeys: String, CodingKey {
         case url
         case htmlURL = "html_url"
         case commit
+    }
+    
+    enum CommitCodingKeys: String, CodingKey {
+        case author
+    }
+    
+    enum AuthorCodingKeys: String, CodingKey {
+        case name, email, date
+    }
+    
+    init(form decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        url = try container.decode(URL.self, forKey: .url)
+        htmlURL = try container.decode(URL.self, forKey: .htmlURL)
+        
+        let commit = try container.nestedContainer(keyedBy: CommitCodingKeys.self, forKey: .commit)
+        let author = try commit.nestedContainer(keyedBy: AuthorCodingKeys.self, forKey: .author)
+        
+        name = try author.decode(String.self, forKey: .name)
+        email = try author.decode(String.self, forKey: .email)
+        date = try author.decode(Date.self, forKey: .date)
     }
 }
 
@@ -44,9 +58,10 @@ class ViewController: NSViewController {
         }
         do {
             let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601 // Standard date format
             let contents = try Data(contentsOf: url)
             let decoded = try decoder.decode([Entry].self, from: contents)
-            print(decoded[0].commit.author.email)
+            print(decoded[0].date)
         } catch {
             print("Unable to parse JSON: \(error.localizedDescription)")
         }
