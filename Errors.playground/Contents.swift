eb25e577028eb7b3f2353bbd9e44fa462620c535
gaperlinski <gaperlinski@gmail.com>
2018-03-15 10:10:59 +0100

    asserts, preconditions, fatalError

--- Errors.playground/Contents.swift
+++ Errors.playground/Contents.swift
@@ -2,12 +2,27 @@
 
 import UIKit
 
-let fm =  FileManager.default
-let bundle = Bundle.main
+assert(1 == 1, "Maths failure")
 
-let contents = fm.contentsOfDirectory(atPath: bundle.resourcePath!)
+let pathToDebugFile = ""
 
-let savedText = (try? String(contentsOfFile: "myfile.txt")) ?? "Example text here"
+func saveError(message: String, file: String = #file, line: Int = #line) -> String {
+    _ = try? message.write(toFile: pathToDebugFile, atomically: true, encoding: .utf8)
+    return message
+}
 
-let str = ""
-_ = try? str.write(to: URL("file://example.txt")!, atomically: true, encoding: .utf8)
+assert(1 == 2, saveError(message: "Fatal error: maths be bad")) // don't fail at runtime
+precondition(1 == 2, saveError(message: "Fatal error: maths be bad")) // fail at runtime
+
+fatalError("This will always crash")
+
+class MyCell: UITableViewCell {}
+
+class ViewController: UITableViewController {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath) as? MyCell else {
+            fatalError("Failed to dequeue MyCell")
+        }
+        return cell
+    }
+}
